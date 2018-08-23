57260be2eedab55dfe2ede913e6cf7f0fd49c84c
luisrocag <32248714+luisrocag@users.noreply.github.com>
2018-08-23 15:14:04 -0500

    Interpolacion de Lagrange

---
+++ lagrange.m
@@ -0,0 +1,15 @@
+﻿function [C,L]=lagrange(X,Y)
+w=length(X);
+n=w-1;
+L=zeros(w,w);
+for k=1:n+1
+   V=1;
+   for j=1:n+1
+     if k!=j
+        V=conv(V,poly(X(j)))/(X(k)-X(j));
+     endif
+   endfor
+   L(k,:)=V;
+endfor
+C=Y*L;
+endfunction
