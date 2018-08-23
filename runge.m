23e045a6e7e360756172beac6939de96c624e5f6
luisrocag <32248714+luisrocag@users.noreply.github.com>
2018-08-23 15:24:32 -0500

    Ejemplo "Efecto Runge"

---
+++ runge.m
@@ -0,0 +1,10 @@
+﻿frunge=@(x)1./(1+25*x.^2);
+n=15;
+X=-1:1/n:1;
+%X=cos((2*[1:n]-1)*pi/(2*n));
+Y=frunge(X);
+xx=linspace(-1,1,50);
+p=lagrange(X, Y);
+zz=polyval(p, xx);
+yy=frunge(xx);
+plot(xx,zz,'k',xx,yy,'r',X,Y,'+')
