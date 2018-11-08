c3cc943c5e4e5515127a86505733d693afbc9ec8
Luis Roca <32248714+luisrocag@users.noreply.github.com>
2018-11-08 14:53:26 -0500

    Update edoTaylor.m

--- edoTaylor.m
+++ edoTaylor.m
@@ -1,4 +1,3 @@
-﻿1;
 function z=edoTaylor(f,fx,ft,a,xa,b,h)
 t=a:h:b;
 z=zeros(size(t));
@@ -10,8 +9,8 @@ for i=1:length(t)-1
     z(i+1)=z(i) + h*F3+0.5*h^2*(F2+F3*F1);;
 end
 end
-f_edo=@(x,t) -x;fx=@(x,t) -1;ft=@(x,t) 0;
-x0 = 1; t0 = 0;
-% resuelve x'=-x, x(0)=1
-tf= 10;h=0.1;
-z=edoTaylor(f_edo,fx,ft,t0,x0,tf,h);
+% f_edo=@(x,t) -x;fx=@(x,t) -1;ft=@(x,t) 0;
+% x0 = 1; t0 = 0;
+%% resuelve x'=-x, x(0)=1
+% tf= 10;h=0.1;
+% z=edoTaylor(f_edo,fx,ft,t0,x0,tf,h);
