64bb0158a9420844abdf8183611c07b449729b9d
Luis Roca <32248714+luisrocag@users.noreply.github.com>
2018-11-08 14:51:55 -0500

    Update edoHeun.m

--- edoHeun.m
+++ edoHeun.m
@@ -1,4 +1,3 @@
-﻿1;
 function z=edoHeun(f,a,xa,b,h)
 t=a:h:b;
 z=zeros(size(t));
@@ -8,11 +7,11 @@ for i=1:length(t)-1
     F2= h*f(z(i)+F1,t(i+1));
     z(i+1)=z(i) + 0.5*(F1+F2);
 end
-end
+endfunction
 
-f_edo=@(x,t) -x;
-x0 = 1; t0 = 0;
-% resuelve x'=-x, x(0)=1
-tf= 10;h=0.1;
-z=edoHeun(f_edo,t0,x0,tf,h);
+% f_edo=@(x,t) -x;
+% x0 = 1; t0 = 0;
+%% resuelve x'=-x, x(0)=1
+% tf= 10;h=0.1;
+% z=edoHeun(f_edo,t0,x0,tf,h);
 
